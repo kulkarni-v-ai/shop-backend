6af8e2b14c675eb6762a2bfaade1fce649d174c5
kulkarni-v-ai <vineetskulk@gmail.com>
2026-02-21 05:29:38 +0530

    Logical JWT changes

--- server.js
+++ server.js
@@ -65,8 +65,9 @@ const PORT = process.env.PORT || 5000;
 
 mongoose
   .connect(process.env.MONGO_URI)
-  .then(() => {
+  .then(async () => {
     console.log("MongoDB connected");
+    await createSuperAdmin();
     app.listen(PORT, () => {
       console.log(`Server running on port ${PORT}`);
     });
